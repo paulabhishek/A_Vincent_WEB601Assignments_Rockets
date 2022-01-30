609b9ce989b636e0e33557c252ce17208fb77d1c
Abhishek Paul <32842796+paulabhishek@users.noreply.github.com>
2022-01-30 18:05:22 -0500

    starting Content-card

---
+++ A-Vincent-MyFavouriteRockets/src/app/helper-files/ content-list.ts
@@ -0,0 +1,37 @@
+import {Content} from "./content-interface";
+
+export class ContentList{
+private _items: Content[];
+
+constructor() {
+  this._items = [];
+}
+get items(): Content[]{
+  return this._items;
+}
+
+set items(newItem: Content[]){
+  this._items = newItem;
+}
+
+addItem(newItem:Content){
+  this._items.push(newItem);
+}
+
+itemCount(){
+  return this._items.length;
+}
+
+outputRockets(id:number):string{
+  if(id >= this.itemCount()){
+    return `index ${id} is empty`;
+  } else{
+    return `<h1>Title: ${this._items[id].title}</h1>
+            <h2>Creator: ${this._items[id].creator}</h2>
+            <img width="500" height="600" src ='${this._items[id].imgURL}' alt='${this._items[id].title}' />
+            <h2>Type: ${this._items[id].type}</h2>
+            <h5>Description: ${this._items[id].description}</h5>`
+
+  }
+}
+}
