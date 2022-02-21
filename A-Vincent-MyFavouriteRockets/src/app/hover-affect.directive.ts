66e9774b6c7ee804c7218c32c40395e31a5a4243
Abhishek Paul <32842796+paulabhishek@users.noreply.github.com>
2022-02-20 21:01:59 -0500

    Added directives to tag and type

--- A-Vincent-MyFavouriteRockets/src/app/hover-affect.directive.ts
+++ A-Vincent-MyFavouriteRockets/src/app/hover-affect.directive.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
+  @Input() style?: string;
 
-  constructor() { }
+
+  constructor(private e:ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter(){
+    if(this.style == "bold"){
+      this.e.nativeElement.style.textDecoration="bold";
+    }
+    else if(this.style == "underline"){
+      this.e.nativeElement.style.textDecoration="underline";
+    }
+    else {
+      this.e.nativeElement.style.borderColor = 'black';
+      this.e.nativeElement.style.borderWidth="3px";
+    }
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.e.nativeElement.removeAttribute('style');
+  }
+  ngOnInit() {
+    this.e.nativeElement.style.backgroundColor = this.e;
+  }
+
 
 }
+
+
