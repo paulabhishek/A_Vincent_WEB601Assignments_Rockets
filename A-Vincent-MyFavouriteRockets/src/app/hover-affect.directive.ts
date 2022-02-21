309f8bd34b1709255b93919a42f329ba4253dec9
Abhishek Paul <32842796+paulabhishek@users.noreply.github.com>
2022-02-20 21:35:07 -0500

    Added directives to tag and type - debugging

--- A-Vincent-MyFavouriteRockets/src/app/hover-affect.directive.ts
+++ A-Vincent-MyFavouriteRockets/src/app/hover-affect.directive.ts
@@ -4,30 +4,31 @@ import { Directive, ElementRef, Input, HostListener } from '@angular/core';
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
-  @Input() style?: string;
+  @Input() line?: string;
 
 
   constructor(private e:ElementRef) { }
 
   @HostListener('mouseenter') onMouseEnter(){
-    if(this.style == "bold"){
-      this.e.nativeElement.style.textDecoration="bold";
-    }
-    else if(this.style == "underline"){
-      this.e.nativeElement.style.textDecoration="underline";
-    }
-    else {
-      this.e.nativeElement.style.borderColor = 'black';
-      this.e.nativeElement.style.borderWidth="3px";
-    }
+    // if(this.style == "bold"){
+    //   this.e.nativeElement.style.textDecoration="bold";
+    // }
+    // else if(this.style == "underline"){
+    //   this.e.nativeElement.style.textDecoration="underline";
+    // }
+    // else {
+    //   this.e.nativeElement.style.borderColor = 'black';
+    //   this.e.nativeElement.style.borderWidth="3px";
+    // }
+    this.underlineElement(this.line);
   }
 
-  @HostListener('mouseleave') onMouseLeave(){
-    this.e.nativeElement.removeAttribute('style');
-  }
-  ngOnInit() {
-    this.e.nativeElement.style.backgroundColor = this.style;
-  }
+  // @HostListener('mouseleave') onMouseLeave(){
+  //   this.e.nativeElement.removeAttribute('style');
+  // }
+  // ngOnInit() {
+  //   this.e.nativeElement.style.backgroundColor = this.line;
+  // }
 
 
 }
