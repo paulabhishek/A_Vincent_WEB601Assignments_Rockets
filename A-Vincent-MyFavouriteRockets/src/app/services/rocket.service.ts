643ba1818a3f1560c772bdf3879fb6896a2b6dae
Abhishek Paul <32842796+paulabhishek@users.noreply.github.com>
2022-03-13 23:13:51 -0400

    Message component and service

--- A-Vincent-MyFavouriteRockets/src/app/services/rocket.service.ts
+++ A-Vincent-MyFavouriteRockets/src/app/services/rocket.service.ts
@@ -2,29 +2,33 @@ import { Injectable } from '@angular/core';
 import {Content} from "../helper-files/content-interface";
 import {ROCKETS} from "../helper-files/contentDb";
 import {Observable, of} from "rxjs";
+import {MessagesService} from "./messages.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RocketService {
 
-  constructor() { }
-  getContent(): Observable<Content[]> {
-    const rockets = of(ROCKETS);
-    return rockets;
-  }
+  constructor(private messagesService: MessagesService) { }
+
+  // getContent(): Observable<Content[]> {
+  //   const rockets = of(ROCKETS);
+  //   return rockets;
+  // }
 
   getContentObs(): Observable<Content[]> { //async function
     return of(ROCKETS);
   }
 
   getIdContent(id: any): Observable<Content>{
-    const message = ROCKETS[id];
+    //const message = ROCKETS[id];
     if (id < ROCKETS.length){
+      this.messagesService.add("Content array loaded!");
+      this.messagesService.add('Content item at ID : ' );
       console.log("Success !",ROCKETS);
     } else{
       console.log("Failed !");
-      //this.DisplayError();
+
     }
     return of(ROCKETS[id]);
   }
