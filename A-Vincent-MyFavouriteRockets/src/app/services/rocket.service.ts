d46f23324f5c38a6484b5ea8ba4c2fef7213da5a
Abhishek Paul <32842796+paulabhishek@users.noreply.github.com>
2022-03-27 16:31:10 -0400

    added new http service

--- A-Vincent-MyFavouriteRockets/src/app/services/rocket.service.ts
+++ A-Vincent-MyFavouriteRockets/src/app/services/rocket.service.ts
@@ -3,18 +3,20 @@ import {Content} from "../helper-files/content-interface";
 import {ROCKETS} from "../helper-files/contentDb";
 import {Observable, of} from "rxjs";
 import {MessagesService} from "./messages.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RocketService {
+  private httpOptions = {
+    headers: new HttpHeaders({'content-type' : 'application/json'})
+  }
+  constructor(private messagesService: MessagesService, private http: HttpClient) { }
 
-  constructor(private messagesService: MessagesService) { }
-
-  // getContent(): Observable<Content[]> {
-  //   const rockets = of(ROCKETS);
-  //   return rockets;
-  // }
+  getContent(): Observable<Content[]> {
+    return this.http.get<Content[]>("api/content");
+  }
 
   getContentObs(): Observable<Content[]> { //async function
     return of(ROCKETS);
@@ -31,5 +33,15 @@ export class RocketService {
       this.messagesService.add('Content not available');
     }
     return of(ROCKETS[id]);
+  }
+
+  addContent(newContentItem: Content): Observable<Content>{
+    return this.http.post<Content>("api/content", newContentItem, this.httpOptions);
+    console.log('Adding new content', newContentItem);
+    this.messagesService.add('Adding new content' + newContentItem);
+  }
+
+  updateContent(contentItem: Content): Observable<any>{
+    return this.http.put("api/content", contentItem, this.httpOptions);
   }
 }
