d37ab19cedc4a04235553429f7b834760b561554
Abhishek Paul <32842796+paulabhishek@users.noreply.github.com>
2022-03-27 16:46:41 -0400

    fixing bugs: added imports in app module

--- A-Vincent-MyFavouriteRockets/src/app/app.module.ts
+++ A-Vincent-MyFavouriteRockets/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { FilterTypePipe } from './filter-type.pipe';
 import { HoverAffectDirective } from './hover-affect.directive';
 import { AppMessagesComponent } from './app-messages/app-messages.component';
-import { ModifyContentComponentComponent } from './modify-content-component/modify-content-component.component';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { InMemoryDataService } from './services/in-memory-data.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ModifyContentComponentComponent } from './modify-content-component/modify-content-component.component';
 
 
 @NgModule({
@@ -23,7 +25,11 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+      delay: 2000
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
