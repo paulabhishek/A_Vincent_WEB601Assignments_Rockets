ab91a79f3786f9e939674cfbdebf5831aee2653a
paulabhishek <1998vabhishekpaul@gmail.com>
2022-04-10 20:48:43 -0400

    used this cmd "ng generate module app-routing --flat" to create routing module

--- A-Vincent-MyFavouriteRockets/src/app/app-routing.module.ts
+++ A-Vincent-MyFavouriteRockets/src/app/app-routing.module.ts
@@ -4,3 +4,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentDetailComponent } from './content-detail/content-detail.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const  routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/content",
+    pathMatch: "full"
+  },
+  {
+    path: "content",
+    component: ContentDetailComponent
+  }
+  // {
+  //   path: "**",
+  //   component: PageNotFoundComponent
+  // },
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
