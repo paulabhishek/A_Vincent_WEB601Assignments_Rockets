c857b7f332b0436aa85d0f2bcfdf4cab735abc04
paulabhishek <1998vabhishekpaul@gmail.com>
2022-04-18 11:07:21 -0400

    completed bonus

--- A-Vincent-MyFavouriteRockets/src/app/app.component.ts
+++ A-Vincent-MyFavouriteRockets/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {ApplicationRef, Component, OnInit} from '@angular/core';
 import {Content} from "./helper-files/content-interface";
 import {RocketService} from "./services/rocket.service";
 import {LogUpdateService} from "./log-update.service";
+import {concat, first, interval} from "rxjs";
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
@@ -12,16 +14,25 @@ import {LogUpdateService} from "./log-update.service";
 export class AppComponent implements OnInit{
   title = 'A-Vincent-MyFavouriteRockets';
   getIdContent?: Content;
-  constructor(private rocketService: RocketService, private logService: LogUpdateService){}
+
+  constructor(private rocketService: RocketService, private logService: LogUpdateService, private appRef: ApplicationRef, private updates: SwUpdate){}
 
   ngOnInit(): void {
-    this.logService.init();
+    if(navigator.serviceWorker){
+      this.logService.init();
 
+      const appIsStable$ = this.appRef.isStable.pipe(
+        first(isStable => isStable === true));
+      const everyHour$ = interval(1 * 60 * 60 * 1000 /2);
+      const everyHourOnceAppIsStable$ =
+        concat(appIsStable$, everyHour$);
+      everyHourOnceAppIsStable$.subscribe(
+        () => this.updates.checkForUpdate());
+    }
   }
 
   btn(id:any) {
     this.rocketService.getIdContent(id).subscribe((getContent: any) => this.getIdContent = getContent);
   }
-
 
 }
